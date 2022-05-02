ab2012951adfda623c7b95876fae691c2e0d6db9
Philip Krogel <philip@kro.dev>
2022-05-02 08:04:19 -0400

    wip

---
+++ app.vue
@@ -0,0 +1,37 @@
+<template>
+  <Head>
+    <Link
+      href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&amp;display=swap"
+      rel="stylesheet"
+    />
+    <Link
+      href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i&amp;display=swap"
+      rel="stylesheet"
+    />
+  </Head>
+  <div class="min-h-screen overflow-hidden">
+    <div class="relative overflow-hidden bg-primary from-gray-50 to-white">
+      <div class="relative">
+        <default-header>
+          <template #extraHead>
+            <main class="w-full bg-primary">
+              <slot name="layoutExtraHead"></slot>
+            </main>
+          </template>
+        </default-header>
+      </div>
+    </div>
+
+    <NuxtPage />
+    <default-footer :prefooter="showPrefooter"></default-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DefaultLayout",
+  props: ["showPrefooter"],
+};
+</script>
+
+<style scoped></style>
---
+++ pages/locations/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="content" class="bg-slate-500 pt-[25%] md:pt-[10%] pb-12 md:pb-32">
+    <div class="section-heading text-center pt-8 pb-12">
+      <h1 class="text-4xl md:text-6xl uppercase font-black tracking-wide">
+        Select Location
+      </h1>
+      <p></p>
+    </div>
+    <div class="section-heading pt-8 pb-12 w-full">
+      <div
+        class="grid grid-flow-row-dense grid-cols-1 md:grid-cols-2 md:grid-rows-3 mx-24"
+      >
+        <div class="w-full mb-4" v-for="{club_name, club_id} in locations">
+          <a
+            class="btn w-[95%] btn-outline rounded-[0px] hover:text-secondary text-2xl"
+            :href="'/clubs/' + club_id"
+            >{{ club_name }}</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const {data: locations} = await useFetch('/api/locations')
+</script>

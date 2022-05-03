74ca890cc5c92ade9377a8099bdca59c92506b0b
Philip Krogel <philip@kro.dev>
2022-05-03 12:56:17 -0400

    before prettier

--- pages/news.vue
+++ pages/news.vue
@@ -2,8 +2,13 @@
   <div class="mt-32 flex flex-col items-center gap-32">
     <h1 class="text-6xl">LATEST NEWS</h1>
 
-    <article v-for="post in posts" class="flex flex-col gap-2 w-full max-w-[600px] border border-1 rounded-lg border-opacity-50 border-white p-8">
-      <h1 class="font-bold text-2xl uppercase leading-loose">{{ post.title }}</h1>
+    <article
+      v-for="post in posts"
+      class="flex flex-col gap-2 w-full max-w-[600px] border border-1 rounded-lg border-opacity-50 border-white p-8"
+    >
+      <h1 class="font-bold text-2xl uppercase leading-loose">
+        {{ post.title }}
+      </h1>
       <p class="text-lg">{{ post.body }}</p>
     </article>
     <div class="bg-red text-white p-8">
@@ -13,7 +18,6 @@
 </template>
 
 <script setup>
-// import NewsItem from "./Partials/NewsItem";
 const baseUrl = "https://dummyjson.com/posts";
 
 const skip = ref(0);
